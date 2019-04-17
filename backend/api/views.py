c9c01cbfae4dbbeb3c1d3cc1f229af31b4b1f60b
Sameet Sapra <sameetandpotatoes@gmail.com>
2019-04-17 14:50:44 -0500

    Confirm that FB graph api requests work

--- backend/api/views.py
+++ backend/api/views.py
@@ -72,15 +72,15 @@ def login(request):
     # Validate token
     token = json_body['token']
     request.session[SESSION_TOKEN_KEY] = token
-    if settings.DEBUG:
-        name = 'Lenny'
-        email = 'lpitt2@illinois.edu'
-    else:
-        graph = facebook.GraphAPI(token)
-        args = {'fields' : 'id,name,email', }
-        profile = graph.get_object('me', **args)
-        name = profile['name']
-        email = profile['email']
+    # if settings.DEBUG:
+    #     name = 'Lenny'
+    #     email = 'lpitt2@illinois.edu'
+    # else:
+    graph = facebook.GraphAPI(token)
+    args = {'fields' : 'id,name,email', }
+    profile = graph.get_object('me', **args)
+    name = profile['name']
+    email = profile['email']
 
     m, created = models.User.objects.get_or_create(
         email=email,
@@ -145,7 +145,8 @@ def interaction(request):
 
     Output:
     {
-        success: bool
+        logger_id: int, # Who was logging
+        loggee_id: int, # Who was logged
     }
     """
 
@@ -184,6 +185,7 @@ def interaction(request):
             })
         return JsonResponse(interaction_json, status=200)
     elif request.method == 'POST':
+        print(json_body)
         ret = validate(json_body, interaction_post_schema)
         if ret is not None:
             return ret
@@ -195,17 +197,17 @@ def interaction(request):
         entry.time_of_day = json_body['time']
         entry.social_context = json_body['social']
         entry.interaction_medium = json_body['medium']
-        entry.content_class = json_body['content']
         entry.other_loggable_text = json_body.get('description', '')
         entry.save()
-        return JsonResponse({"success": "true"}, status=200)
+        ret = json.dumps(dict(logger_id=entry.logger_id, loggee_id = entry.loggee_id))
+        return HttpResponse(json.dumps(ret), content_type='application/json', status=200)
 
 @csrf_exempt
 @restrict_function(allowed=['GET', 'POST'])
 def friends(request):
     """
     Get:
-    Retrieves all friends for logged in user
+    Retrieves all friends for logged in users
 
     Input:
     Empty Body
